76a5f5c2dd1cb8996f2d25ff2088277fc886041e
khushi-rai-codes <kr14052006@gmail.com>
2026-05-27 20:57:50 +0530

    Create queue_using_linked_list.c

---
+++ queue_using_linked_list.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *front = NULL;
+struct Node *rear = NULL;
+void enqueue(int value)
+{
+    struct Node *newNode;
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    if(rear == NULL)
+    {
+        front = rear = newNode;
+    }
+    else
+    {
+        rear->next = newNode;
+        rear = newNode;
+    }
+    printf("%d inserted into queue\n", value);
+}
+void dequeue()
+{
+    if(front == NULL)
+    {
+        printf("Queue Underflow\n");
+        return;
+    }
+    struct Node *temp = front;
+    printf("%d removed from queue\n", front->data);
+    front = front->next;
+    if(front == NULL)
+        rear = NULL;
+    free(temp);
+}
+void display()
+{
+    struct Node *temp = front;
+    if(temp == NULL)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue elements:\n");
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+    dequeue();
+    display();
+    return 0;
+}
