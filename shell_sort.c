e6d5e186d8f8cdd444d9c3c25dea5eb7ebb97e6f
Khushi <kr14052006@gmail.com>
2026-07-16 20:54:50 +0530

    Create shell_sort.c

---
+++ shell_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void shellSort(int arr[], int n)
+{
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+            {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    shellSort(arr, n);
+    printf("Sorted Array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
