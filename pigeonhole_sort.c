6240939414c9b8cba3e8bf1cd58d5d4fe1734728
Khushi <kr14052006@gmail.com>
2026-07-17 22:02:48 +0530

    Create pigeonhole_sort.c

---
+++ pigeonhole_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void pigeonholeSort(int arr[], int n)
+{
+    int min = arr[0], max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    int range = max - min + 1;
+    int holes[range];
+    for (int i = 0; i < range; i++)
+        holes[i] = 0;
+    for (int i = 0; i < n; i++)
+        holes[arr[i] - min]++;
+    int index = 0;
+    for (int i = 0; i < range; i++)
+    {
+        while (holes[i]--)
+            arr[index++] = i + min;
+    }
+}
+int main()
+{
+    int arr[] = {8, 3, 2, 7, 4, 6, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    pigeonholeSort(arr, n);
+    printf("Sorted Array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
